b20ecdb175d3bce0de542903d917271e1a0c8390
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2010-02-03 10:21:49 +0000

    test for multiple replacements per config added and simplified verify script. also removing the test for issue 25 as prefered handling is with the multiple replacements

--- test-plugin-use/verifyReplacement.sh
+++ test-plugin-use/verifyReplacement.sh
@@ -1,90 +1,45 @@
 #!/bin/sh
 
+checkForTokens() {
+	FILE=$1
+	TOKEN=$2
+	
+	if [ `grep -c $TOKEN $FILE` = "0" ]; then
+		echo "$FILE $TOKEN removed"
+	else
+		echo "$FILE failed"
+		exit 1
+	fi
+	
+	if [ $# = 3 ]; then
+		VALUE=$3		
+		if [ `grep -c $VALUE $FILE` = "1" ]; then
+			echo "$FILE $VALUE found"
+		else
+			echo "$FILE $VALUE not found"
+			exit 1
+		fi
+	fi
+}
+
 echo "Executing clean and replacement"
 mvn clean test
 
 echo ""
 echo "Checking contents"
+
 for f in target/classes/*.txt; do
-	if [ `grep -c value $f` -ge "1" ]; then
-		echo "$f value okay"
-	else
-		echo "$f failed"
-		exit 1
-	fi
-	
-	if [ `grep -c token $f` = "0" ]; then
-		echo "$f no longer contains tokens"
-	else
-		echo "$f failed"
-		exit 1
-	fi
+	checkForTokens $f token value
 done
 
-for f in target/classes/nesteddir/*.txt; do
-	if [ `grep -c value $f` -ge "1" ]; then
-		echo "$f value okay"
-	else
-		echo "$f failed"
-		exit 1
-	fi
-	
-	if [ `grep -c token $f` = "0" ]; then
-		echo "$f no longer contains tokens"
-	else
-		echo "$f failed"
-		exit 1
-	fi
-done
-
-if [ `grep -c token target/simple-outputfile-remove.txt` = "0" ]; then
-	echo "target/simple-outputfile-remove.txt okay"
-else
-	echo "target/simple-outputfile-remove.txt failed"
-	exit 1
-fi
-
-if [ `grep -c token target/multiple-tokens-for-map-outputfile.txt` = "0" ]; then
-	echo "target/multiple-tokens-for-map-outputfile.txt no longer contains tokens"
-else
-	echo "target/multiple-tokens-for-map-outputfile.txt failed"
-	exit 1
-fi
-
-if [ `grep -c value1 target/multiple-tokens-for-map-outputfile.txt` = "1" ]; then
-	echo "target/multiple-tokens-for-map-outputfile.txt value1 okay"
-else
-	echo "target/multiple-tokens-for-map-outputfile.txt failed"
-	exit 1
-fi
-
-if [ `grep -c value2 target/multiple-tokens-for-map-outputfile.txt` = "1" ]; then
-	echo "target/multiple-tokens-for-map-outputfile.txt value2 okay"
-else
-	echo "target/multiple-tokens-for-map-outputfile.txt failed"
-	exit 1
-fi
-
-#new directory creation stopped was not working, now resolved - Issue 22
-if [ `grep -c value target/classes/newdir/simple-outputfile.txt` = "1" ]; then
-	echo "target/classes/newdir/simple-outputfile.txt value okay"
-else
-	echo "target/classes/newdir/simple-outputfile.txt failed"
-	exit 1
-fi
-
-#log.xml test - Issue 25
-if [ `grep -c '<level value="error" />' target/classes/log4j.xml` = "1" ]; then
-	if [ `grep -c '<level value="warn" />' target/classes/log4j.xml` = "1" ]; then
-		echo "target/classes/log4j.xml failed"
-		exit 1
-	else
-		echo "target/classes/log4j.xml value okay"
-	fi
-else
-	echo "target/classes/log4j.xml failed"
-	exit 1
-fi
+checkForTokens target/classes/nesteddir/multiple-files1.txt token value
+checkForTokens target/classes/nesteddir/multiple-files2.txt token value
+checkForTokens target/simple-outputfile-remove.txt token
+checkForTokens target/multiple-tokens-for-map-outputfile.txt token1 value1
+checkForTokens target/multiple-tokens-for-map-outputfile.txt token2 value2
+checkForTokens target/classes/newdir/simple-outputfile.txt token value
+checkForTokens target/classes/special/multiple-tokens-to-replace.txt token1 value1
+checkForTokens target/classes/special/multiple-tokens-to-replace.txt token2 value2
 
 echo ""
 echo "Replacer plugin is okay."
