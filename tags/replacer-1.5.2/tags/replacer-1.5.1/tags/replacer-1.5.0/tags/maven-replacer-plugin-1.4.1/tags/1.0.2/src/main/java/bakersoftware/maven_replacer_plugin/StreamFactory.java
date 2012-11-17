b7d60459ed7f6a306e239da920565976a15d5ff5
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2012-11-17 01:38:45 +0000

    clean up

--- tags/replacer-1.5.2/tags/replacer-1.5.1/tags/replacer-1.5.0/tags/maven-replacer-plugin-1.4.1/tags/1.0.2/src/main/java/bakersoftware/maven_replacer_plugin/StreamFactory.java
+++
@@ -1,9 +0,0 @@
-package bakersoftware.maven_replacer_plugin;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public interface StreamFactory {
-	InputStream getNewInputStream();
-	OutputStream getNewOutputStream();
-}
