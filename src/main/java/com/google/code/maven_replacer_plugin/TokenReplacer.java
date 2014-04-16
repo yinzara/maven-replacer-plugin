918bd431708a10641a3315cf4b6d9545fb678913
baker.steven.83 <baker.steven.83@gmail.com>
2014-04-16 08:40:50 +0000

    restoring after release

--- src/main/java/com/google/code/maven_replacer_plugin/TokenReplacer.java
+++ src/main/java/com/google/code/maven_replacer_plugin/TokenReplacer.java
@@ -1,19 +1,11 @@
 package com.google.code.maven_replacer_plugin;
 
-import static org.apache.commons.lang.StringUtils.defaultString;
-import static org.apache.commons.lang.StringUtils.isEmpty;
-
 import java.util.regex.Pattern;
 
-public class TokenReplacer implements Replacer {
-	public String replace(String content, Replacement replacement, boolean regex, int regexFlags) {
-		if (regex) {
-			return replaceRegex(content, replacement.getToken(), replacement.getValue(), regexFlags);
-		}
-		return replaceNonRegex(content, replacement.getToken(), replacement.getValue());
-	}
-	
-	private String replaceRegex(String content, String token, String value, int flags) {
+public class TokenReplacer {
+	public String replaceRegex(String contents, String token, String value, int flags) {
+		String valueToReplaceWith = value == null ? "" : value;
+		
 		final Pattern compiledPattern;
 		if (flags == PatternFlagsFactory.NO_FLAGS) {
 			compiledPattern = Pattern.compile(token);
@@ -21,14 +13,24 @@ public class TokenReplacer implements Replacer {
 			compiledPattern = Pattern.compile(token, flags);
 		}
 		
-		return compiledPattern.matcher(content).replaceAll(defaultString(value));
+		return compiledPattern.matcher(contents).replaceAll(valueToReplaceWith);
 	}
 
-	private String replaceNonRegex(String content, String token, String value) {
-		if (isEmpty(content)) {
-			return content;
+	public String replaceNonRegex(String input, String token, String value) {
+		String valueToReplaceWith = value == null ? "" : value;
+		if (input.length() == 0) {
+			return input;
 		}
 
-		return content.replace(token, defaultString(value));
+		StringBuffer buffer = new StringBuffer();
+		int start = 0;
+		int end = 0;
+		while ((end = input.indexOf(token, start)) >= 0) {
+			buffer.append(input.substring(start, end));
+			buffer.append(valueToReplaceWith);
+			start = end + token.length();
+		}
+		return buffer.append(input.substring(start)).toString();
 	}
+
 }
--- src/main/java/com/google/code/maven_replacer_plugin/TokenValueMapFactory.java
+++ src/main/java/com/google/code/maven_replacer_plugin/TokenValueMapFactory.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import com.google.code.maven_replacer_plugin.file.FileUtils;
 
@@ -13,7 +12,9 @@ import com.google.code.maven_replacer_plugin.file.FileUtils;
 public class TokenValueMapFactory {
 
 	private static final char SEPARATOR_ESCAPER = '\\';
+
 	private static final char SEPARATOR = '=';
+
 	private static final String COMMENT_PREFIX = "#";
 
 	private final FileUtils fileUtils;
@@ -21,78 +22,62 @@ public class TokenValueMapFactory {
 	public TokenValueMapFactory(FileUtils fileUtils) {
 		this.fileUtils = fileUtils;
 	}
-	
-	public List<Replacement> replacementsForVariable(String variable, boolean commentsEnabled, boolean unescape, String encoding) {
-		StringTokenizer tokenizer = new StringTokenizer(variable, ",");
-		String fragment = null;
-		List<Replacement> replacements = new ArrayList<Replacement>();
-		while (tokenizer.hasMoreTokens()) {
-			fragment = tokenizer.nextToken();
-			if (ignoreFragment(fragment, commentsEnabled)) {
-				continue;
-			}
 
-			appendReplacement(replacements, fragment, unescape, encoding);
-		}
-		return replacements;
-	}
-
-	public List<Replacement> replacementsForFile(String tokenValueMapFile, boolean commentsEnabled, 
-			boolean unescape, String encoding) 
-		throws IOException {
-		String contents = fileUtils.readFile(tokenValueMapFile, encoding);
+	public List<Replacement> contextsForFile(String tokenValueMapFile, boolean commentsEnabled) throws IOException {
+		String contents = fileUtils.readFile(tokenValueMapFile);
 		BufferedReader reader = new BufferedReader(new StringReader(contents));
-
-		String fragment = null;
-		List<Replacement> replacements = new ArrayList<Replacement>();
-		while ((fragment = reader.readLine()) != null) {
-			fragment = fragment.trim();
-			if (ignoreFragment(fragment, commentsEnabled)) {
-				continue;
-			}
-
-			appendReplacement(replacements, fragment, unescape, encoding);
-		}
-		return replacements;
-	}
-	
-	private void appendReplacement(List<Replacement> replacements, String fragment, boolean unescape, String encoding) {
-		StringBuilder token = new StringBuilder();
-		String value = "";
-		boolean settingToken = true;
-		for (int i=0; i < fragment.length(); i++) {
-			if (i == 0 && fragment.charAt(0) == SEPARATOR) {
-				throw new IllegalArgumentException(getNoValueErrorMsgFor(fragment));
-			}
-
-			if (settingToken && !isSeparatorAt(i, fragment)) {
-				token.append(fragment.charAt(i));
-			} else if (isSeparatorAt(i, fragment)) {
-				settingToken = false;
-				continue;
-			} else {
-				value = fragment.substring(i);
-				break;
-			}
-		}
-
-		if (settingToken) {
-			return;
-		}
 		
-		String tokenVal = token.toString().trim();
-		replacements.add(new Replacement(fileUtils, tokenVal, value.trim(), unescape, null, encoding));
+		String line = null;
+		List<Replacement> contexts = new ArrayList<Replacement>();
+		while ((line = reader.readLine()) != null) {
+			line = line.trim();
+			if (ignoreLine(line, commentsEnabled)) {
+				continue;
+			}
+			
+			StringBuilder token = new StringBuilder();
+			String value = "";
+			boolean settingToken = true;
+			for (int i=0; i < line.length(); i++) {
+				if (i == 0 && line.charAt(0) == SEPARATOR) {
+					throw new IllegalArgumentException(getNoValueErrorMsgFor(line) + "1");
+				}
+				
+				if (settingToken && !isSeparatorAt(i, line)) {
+					token.append(line.charAt(i));
+				} else if (isSeparatorAt(i, line)) {
+					settingToken = false;
+					continue;
+				} else {
+					value = line.substring(i);
+					break;
+				}
+			}
+			
+			String tokenVal = token.toString().trim();
+			if (tokenVal.length() == 0 || settingToken) {
+				continue;
+			}
+			value = value.trim();
+			contexts.add(new Replacement(fileUtils, tokenVal, value));
+		}
+		return contexts;
 	}
 
 	private boolean isSeparatorAt(int i, String line) {
 		return line.charAt(i) == SEPARATOR && line.charAt(i - 1) != SEPARATOR_ESCAPER;
 	}
 
-	private String getNoValueErrorMsgFor(String line) {
-		return "No value for token: " + line + ". Make sure that tokens have values in pairs in the format: token=value";
+	private boolean isNotSeparatorCharAt(int i, String line) {
+		return line.charAt(i) != SEPARATOR || (i > 0 && line.charAt(i) == SEPARATOR && line.charAt(i - 1) == SEPARATOR_ESCAPER);
 	}
 
-	private boolean ignoreFragment(String line, boolean commentsEnabled) {
-		return line.length() == 0 || commentsEnabled && line.startsWith(COMMENT_PREFIX);
+	private String getNoValueErrorMsgFor(String line) {
+		return "No value for token: " + line + ". Make sure that " +
+				"tokens have values in pairs in the format: token=value";
+	}
+
+	private boolean ignoreLine(String line, boolean commentsEnabled) {
+		return line.length() == 0 || (commentsEnabled && line.startsWith(COMMENT_PREFIX));
 	}
 }
