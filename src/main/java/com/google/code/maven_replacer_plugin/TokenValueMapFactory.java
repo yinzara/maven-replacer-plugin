6b9c45f9549c4cb8e664b074ff72da40353a5dbe
baker.steven.83 <baker.steven.83@gmail.com>
2014-04-16 08:40:50 +0000

    restoring after release

--- src/main/java/com/google/code/maven_replacer_plugin/TokenValueMapFactory.java
+++ src/main/java/com/google/code/maven_replacer_plugin/TokenValueMapFactory.java
@@ -1,98 +1,45 @@
 package com.google.code.maven_replacer_plugin;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Properties;
 
 import com.google.code.maven_replacer_plugin.file.FileUtils;
 
 
 public class TokenValueMapFactory {
 
-	private static final char SEPARATOR_ESCAPER = '\\';
-	private static final char SEPARATOR = '=';
-	private static final String COMMENT_PREFIX = "#";
-
 	private final FileUtils fileUtils;
 
 	public TokenValueMapFactory(FileUtils fileUtils) {
 		this.fileUtils = fileUtils;
 	}
-	
-	public List<Replacement> replacementsForVariable(String variable, boolean commentsEnabled, boolean unescape, String encoding) {
-		StringTokenizer tokenizer = new StringTokenizer(variable, ",");
-		String fragment = null;
-		List<Replacement> replacements = new ArrayList<Replacement>();
-		while (tokenizer.hasMoreTokens()) {
-			fragment = tokenizer.nextToken();
-			if (ignoreFragment(fragment, commentsEnabled)) {
-				continue;
-			}
 
-			appendReplacement(replacements, fragment, unescape, encoding);
+	public List<ReplacerContext> contextsForFile(String tokenValueMapFile) throws IOException {
+		String contents = fileUtils.readFile(tokenValueMapFile);
+
+		Properties properties = readProperties(contents);
+		List<ReplacerContext> contexts = new ArrayList<ReplacerContext>();
+		for (Object key : properties.keySet()) {
+			String token = String.valueOf(key);
+			String value = properties.getProperty(token);
+			contexts.add(new ReplacerContext(fileUtils, token, value));
 		}
-		return replacements;
+		return contexts;
 	}
 
-	public List<Replacement> replacementsForFile(String tokenValueMapFile, boolean commentsEnabled, 
-			boolean unescape, String encoding) 
-		throws IOException {
-		String contents = fileUtils.readFile(tokenValueMapFile, encoding);
-		BufferedReader reader = new BufferedReader(new StringReader(contents));
-
-		String fragment = null;
-		List<Replacement> replacements = new ArrayList<Replacement>();
-		while ((fragment = reader.readLine()) != null) {
-			fragment = fragment.trim();
-			if (ignoreFragment(fragment, commentsEnabled)) {
-				continue;
-			}
-
-			appendReplacement(replacements, fragment, unescape, encoding);
+	private Properties readProperties(String contents) throws IOException {
+		Properties properties = new Properties();
+		InputStream inputStream = new ByteArrayInputStream(contents.getBytes());
+		try {
+			properties.load(inputStream);
+		} finally {
+			inputStream.close();
 		}
-		return replacements;
-	}
-	
-	private void appendReplacement(List<Replacement> replacements, String fragment, boolean unescape, String encoding) {
-		StringBuilder token = new StringBuilder();
-		String value = "";
-		boolean settingToken = true;
-		for (int i=0; i < fragment.length(); i++) {
-			if (i == 0 && fragment.charAt(0) == SEPARATOR) {
-				throw new IllegalArgumentException(getNoValueErrorMsgFor(fragment));
-			}
-
-			if (settingToken && !isSeparatorAt(i, fragment)) {
-				token.append(fragment.charAt(i));
-			} else if (isSeparatorAt(i, fragment)) {
-				settingToken = false;
-				continue;
-			} else {
-				value = fragment.substring(i);
-				break;
-			}
-		}
-
-		if (settingToken) {
-			return;
-		}
-		
-		String tokenVal = token.toString().trim();
-		replacements.add(new Replacement(fileUtils, tokenVal, value.trim(), unescape, null, encoding));
+		return properties;
 	}
 
-	private boolean isSeparatorAt(int i, String line) {
-		return line.charAt(i) == SEPARATOR && line.charAt(i - 1) != SEPARATOR_ESCAPER;
-	}
-
-	private String getNoValueErrorMsgFor(String line) {
-		return "No value for token: " + line + ". Make sure that tokens have values in pairs in the format: token=value";
-	}
-
-	private boolean ignoreFragment(String line, boolean commentsEnabled) {
-		return line.length() == 0 || commentsEnabled && line.startsWith(COMMENT_PREFIX);
-	}
 }
--- src/main/java/com/google/code/maven_replacer_plugin/file/FileUtils.java
+++ src/main/java/com/google/code/maven_replacer_plugin/file/FileUtils.java
@@ -1,14 +1,15 @@
 package com.google.code.maven_replacer_plugin.file;
 
-import static org.apache.commons.lang.StringUtils.isBlank;
-import static org.apache.commons.lang.StringUtils.isNotBlank;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileUtils {
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
 	public boolean fileNotExists(String filename) {
-		return isBlank(filename) || !new File(filename).exists();
+		return !new File(filename).exists();
 	}
 
 	public void ensureFolderStructureExists(String file) {
@@ -19,47 +20,26 @@ public class FileUtils {
 
 		if (!outputFile.isDirectory()) {
 			File parentPath = new File(outputFile.getParent());
-			if (!parentPath.exists() && !parentPath.mkdirs()) {
-				throw new IllegalStateException("Error creating directory: " + parentPath);
+			if (!parentPath.exists()) {
+				parentPath.mkdirs();
 			}
 		} else {
-			throw new IllegalArgumentException("outputFile cannot be a directory: " + file);
+			throw new IllegalArgumentException("OutputFile cannot be a directory: " + file);
 		}
 	}
 
-	public String readFile(String file, String encoding) throws IOException {
-		if (encoding != null) {
-			return org.apache.commons.io.FileUtils.readFileToString(new File(file), encoding);
-		}
-		return org.apache.commons.io.FileUtils.readFileToString(new File(file));
-	}
-
-	public void writeToFile(String outputFile, String content, String encoding) throws IOException {
-		ensureFolderStructureExists(outputFile);
-		if (encoding != null) { 
-			org.apache.commons.io.FileUtils.writeStringToFile(new File(outputFile), content, encoding);
-		} else {
-			org.apache.commons.io.FileUtils.writeStringToFile(new File(outputFile), content);
-		}
-	}
-	
-	public String createFullPath(String... dirsAndFilename) {
-		StringBuilder fullPath = new StringBuilder();
-		for (int i=0; i < dirsAndFilename.length - 1; i++) {
-			if (isNotBlank(dirsAndFilename[i])) {
-				fullPath.append(dirsAndFilename[i]);
-				fullPath.append(File.separator);
+	public String readFile(String file) throws IOException {
+		StringBuilder contents = new StringBuilder();
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		try {
+			String line = null;
+			while ((line = input.readLine()) != null) {
+				contents.append(line).append(LINE_SEPARATOR);
 			}
+		} finally {
+			input.close();
 		}
-		String last = dirsAndFilename[dirsAndFilename.length - 1];
-		if (last != null) {
-			fullPath.append(last);
-		}
-		
-		return fullPath.toString();
-	}
 
-	public boolean isAbsolutePath(String file) {
-		return new File(file).isAbsolute();
+		return contents.toString().trim();
 	}
 }
