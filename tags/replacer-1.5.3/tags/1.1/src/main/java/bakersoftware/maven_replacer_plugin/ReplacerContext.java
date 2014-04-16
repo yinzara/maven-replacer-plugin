17a3efb59df278d048d40a611638d8a063904c2c
baker.steven.83 <baker.steven.83@gmail.com>
2014-04-16 08:40:50 +0000

    restoring after release

---
+++ tags/replacer-1.5.3/tags/1.1/src/main/java/bakersoftware/maven_replacer_plugin/ReplacerContext.java
@@ -0,0 +1,43 @@
+package bakersoftware.maven_replacer_plugin;
+
+import java.io.IOException;
+
+import bakersoftware.maven_replacer_plugin.file.FileUtils;
+
+public class ReplacerContext {
+	private String token;
+	private String value;
+
+	public ReplacerContext(String token, String value) {
+		this.token = token;
+		this.value = value;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public void setTokenFile(String tokenFile) throws IOException {
+		if (tokenFile != null) {
+			token = new FileUtils().readFile(tokenFile).trim();
+		}
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public void setValueFile(String valueFile) throws IOException {
+		if (valueFile != null) {
+			value = new FileUtils().readFile(valueFile).trim();
+		}
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
