9a361886a7debba055cadc868c1bcc01d6a41d0d
baker.steven.83@gmail.com <baker.steven.83@gmail.com@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2014-04-16 07:17:17 +0000

    [maven-release-plugin]  copy for tag replacer-1.5.3

---
+++ tags/replacer-1.5.3/tags/maven-replacer-plugin-1.3.3/src/test/java/com/google/code/maven_replacer_plugin/ReplacerMojoTest.java
@@ -0,0 +1,282 @@
+package com.google.code.maven_replacer_plugin;
+
+
+import static java.util.Arrays.asList;
+import static junit.framework.Assert.assertSame;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.code.maven_replacer_plugin.file.FileUtils;
+import com.google.code.maven_replacer_plugin.include.FileSelector;
+
+public class ReplacerMojoTest {
+
+	private static final String REGEX_FLAG = "regex flag";
+	private static final String FILE = "file";
+	private static final boolean REGEX = true;
+	private static final String OUTPUT_FILE = "output file";
+	private static final int REGEX_PATTERN_FLAGS = 999;
+	private static final String BASE_DIR = "base dir";
+	private static final String TOKEN_VALUE_MAP = "token value map";
+	private static final String TOKEN_FILE = "token file";
+	private static final String VALUE_FILE = "value file";
+	private static final String TOKEN = "token";
+	private static final String VALUE = "value";
+	private static final String OUTPUT_DIR = "output dir";
+	
+	private FileUtils fileUtils;
+	private TokenReplacer tokenReplacer;
+	private ReplacerFactory replacerFactory;
+	private TokenValueMapFactory tokenValueMapFactory;
+	private FileSelector fileSelector;
+	private PatternFlagsFactory patternFlagsFactory;
+	private ReplacerMojo mojo;
+	private Log log;
+	private List<String> regexFlags;
+	private Replacer replacer;
+
+	@Before
+	public void setUp() throws Exception {
+		fileUtils = mock(FileUtils.class);
+		tokenReplacer = mock(TokenReplacer.class);
+		replacerFactory = mock(ReplacerFactory.class);
+		tokenValueMapFactory = mock(TokenValueMapFactory.class);
+		fileSelector = mock(FileSelector.class);
+		patternFlagsFactory = mock(PatternFlagsFactory.class);
+		log = mock(Log.class);
+		replacer = mock(Replacer.class);
+		regexFlags = asList(REGEX_FLAG);
+		
+		when(replacerFactory.create()).thenReturn(replacer);
+		when(patternFlagsFactory.buildFlags(regexFlags)).thenReturn(REGEX_PATTERN_FLAGS);
+
+		mojo = new ReplacerMojo(fileUtils, tokenReplacer, replacerFactory, tokenValueMapFactory, 
+				fileSelector, patternFlagsFactory) {
+			@Override
+			public Log getLog() {
+				return log;
+			}
+		};
+	}
+	
+	@Test
+	public void shouldReplaceContentsInReplacements() throws Exception {
+		Replacement replacement = mock(Replacement.class);
+		List<Replacement> replacements = asList(replacement);
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setReplacements(replacements);
+		mojo.setFile(FILE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		verify(replacer).replace(replacements, REGEX, BASE_DIR  + "/" + FILE, BASE_DIR + "/" + OUTPUT_FILE, REGEX_PATTERN_FLAGS);
+	}
+	
+	@Test
+	public void shouldReplaceContentsInIncludeAndExcludes() throws Exception {
+		List<String> includes = asList("include");
+		List<String> excludes = asList("exclude");
+		when(fileSelector.listIncludes(BASE_DIR, includes, excludes)).thenReturn(asList(FILE));
+		
+		mojo.setIncludes(includes);
+		mojo.setExcludes(excludes);
+		mojo.setToken(TOKEN);
+		mojo.setValue(VALUE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		assertSame(mojo.getIncludes(), includes);
+		assertSame(mojo.getExcludes(), excludes);
+		verify(replacer).replace(argThat(new ReplacementMatcher(TOKEN, VALUE)), eq(REGEX), eq(BASE_DIR  + "/" + FILE), 
+				eq(BASE_DIR + "/" + FILE), anyInt());
+	}
+	
+	@Test
+	public void shouldReplaceContentsAndWriteToOutputDir() throws Exception {
+		mojo.setFile(FILE);
+		mojo.setToken(TOKEN);
+		mojo.setValue(VALUE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.setOutputDir(OUTPUT_DIR);
+		
+		mojo.execute();
+		verify(replacer).replace(argThat(new ReplacementMatcher(TOKEN, VALUE)), eq(REGEX), eq(BASE_DIR  + "/" + FILE), 
+				eq(BASE_DIR + "/" + OUTPUT_DIR + "/" + FILE), anyInt());
+	}
+	
+	@Test
+	public void shouldReplaceContentsInFilesToIncludeAndExclude() throws Exception {
+		String includes = "include1, include2";
+		String excludes = "exclude1, exclude2";
+		when(fileSelector.listIncludes(BASE_DIR, asList("include1", "include2"), asList("exclude1", "exclude2"))).thenReturn(asList(FILE));
+
+		mojo.setFilesToInclude(includes);
+		mojo.setFilesToExclude(excludes);
+		mojo.setToken(TOKEN);
+		mojo.setValue(VALUE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		assertSame(mojo.getFilesToInclude(), includes);
+		assertSame(mojo.getFilesToExclude(), excludes);
+		verify(replacer).replace(argThat(new ReplacementMatcher(TOKEN, VALUE)), eq(REGEX), eq(BASE_DIR  + "/" + FILE), 
+				eq(BASE_DIR + "/" + FILE), anyInt());
+	}
+	
+	@Test
+	public void shouldReplaceContentsWithTokenValuesInMapWithComments() throws Exception {
+		Replacement replacement = mock(Replacement.class);
+		List<Replacement> replacements = asList(replacement);
+		
+		when(tokenValueMapFactory.contextsForFile(TOKEN_VALUE_MAP, true)).thenReturn(replacements);
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setTokenValueMap(TOKEN_VALUE_MAP);
+		mojo.setFile(FILE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		verify(replacer).replace(replacements, REGEX, BASE_DIR  + "/" + FILE, BASE_DIR + "/" + OUTPUT_FILE, REGEX_PATTERN_FLAGS);
+	}
+	
+	@Test
+	public void shouldReplaceContentsWithTokenValuesInMapWithoutComments() throws Exception {
+		Replacement replacement = mock(Replacement.class);
+		List<Replacement> replacements = asList(replacement);
+		
+		when(tokenValueMapFactory.contextsForFile(TOKEN_VALUE_MAP, false)).thenReturn(replacements);
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setTokenValueMap(TOKEN_VALUE_MAP);
+		mojo.setFile(FILE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.setCommentsEnabled(false);
+		mojo.execute();
+		
+		verify(replacer).replace(replacements, REGEX, BASE_DIR  + "/" + FILE, BASE_DIR + "/" + OUTPUT_FILE, REGEX_PATTERN_FLAGS);
+	}
+	
+	@Test
+	public void shouldReplaceContentsWithTokenAndValue() throws Exception {
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setFile(FILE);
+		mojo.setToken(TOKEN);
+		mojo.setValue(VALUE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		verify(replacer).replace(argThat(new ReplacementMatcher(TOKEN, VALUE)), eq(REGEX), eq(BASE_DIR  + "/" + FILE), 
+				eq(BASE_DIR + "/" + OUTPUT_FILE), eq(REGEX_PATTERN_FLAGS));
+	}
+	
+	@Test
+	public void shouldReplaceContentsWithTokenValuesInTokenAndValueFiles() throws Exception {
+		when(fileUtils.readFile(TOKEN_FILE)).thenReturn(TOKEN);
+		when(fileUtils.readFile(VALUE_FILE)).thenReturn(VALUE);
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setFile(FILE);
+		mojo.setTokenFile(TOKEN_FILE);
+		mojo.setValueFile(VALUE_FILE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		verify(replacer).replace(argThat(new ReplacementMatcher(TOKEN, VALUE)), eq(REGEX), eq(BASE_DIR  + "/" + FILE), 
+				eq(BASE_DIR + "/" + OUTPUT_FILE), eq(REGEX_PATTERN_FLAGS));
+		verify(fileUtils).readFile(TOKEN_FILE);
+		verify(fileUtils).readFile(VALUE_FILE);
+	}
+	
+	@Test
+	public void shouldReplaceContentsInReplacementsInSameFileWhenNoOutputFile() throws Exception {
+		Replacement replacement = mock(Replacement.class);
+		List<Replacement> replacements = asList(replacement);
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setReplacements(replacements);
+		mojo.setFile(FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+		
+		verify(replacer).replace(replacements, REGEX, BASE_DIR  + "/" + FILE, BASE_DIR + "/" + FILE, REGEX_PATTERN_FLAGS);
+	}
+	
+	@Test
+	public void shouldNotReplaceIfIgnoringMissingFilesAndFileNotExists() throws Exception {
+		when(fileUtils.fileNotExists(FILE)).thenReturn(true);
+		mojo.setFile(FILE);
+		mojo.setIgnoreMissingFile(true);
+		
+		mojo.execute();
+		verifyZeroInteractions(replacerFactory);
+		verify(log).info(anyString());
+	}
+
+	@Test
+	public void shouldCreateNewInstancesOfDepenenciesOnConstructor() {
+		new ReplacerMojo();
+	}
+	
+	@Test (expected = MojoExecutionException.class)
+	public void shouldRethrowIOExceptionsAsMojoExceptions() throws Exception {
+		when(fileUtils.readFile(anyString())).thenThrow(new IOException());
+		
+		mojo.setRegexFlags(regexFlags);
+		mojo.setRegex(REGEX);
+		mojo.setFile(FILE);
+		mojo.setTokenFile(TOKEN_FILE);
+		mojo.setValueFile(VALUE_FILE);
+		mojo.setOutputFile(OUTPUT_FILE);
+		mojo.setBasedir(BASE_DIR);
+		mojo.execute();
+	}
+	
+	private static class ReplacementMatcher extends BaseMatcher<List<Replacement>> {
+		private final String token;
+		private final String value;
+
+		public ReplacementMatcher(String token, String value) {
+			this.token = token;
+			this.value = value;
+		}
+
+		public boolean matches(Object arg0) {
+			@SuppressWarnings("unchecked")
+			Replacement replacement = ((List<Replacement>) arg0).get(0);
+			return token.equals(replacement.getToken()) && value.equals(replacement.getValue());
+		}
+
+		public void describeTo(Description arg0) {
+		}
+		
+	}
+}
+
---
+++ tags/replacer-1.5.3/test-plugin-use/src/test/groovy/CheckFilesTest.groovy
@@ -0,0 +1,54 @@
+import org.junit.Test
+
+class CheckFilesTest {
+	
+	def checkForTokensAndValues = { filename, token="token", value="value" ->
+		println "Inspecting file: ${filename}"
+		String content = new File(filename).getText()
+		
+		assert !content.contains(token)
+		println "${filename} ${token} removed"
+		assert content.contains(value)
+		println "${filename} ${value} found"
+	}
+	
+	def checkFileSizeMatches = { filename1, filename2 ->
+		assert new File(filename1).size() == new File(filename2).size()
+		println "File sizes match between ${filename1} and ${filename2}"
+	}
+
+	@Test	
+	void shouldHaveFilesReplaced() {
+		checkFileSizeMatches "src/main/resources/simple.txt", "target/classes/simple.txt"
+		checkFileSizeMatches "src/main/resources/largefile.txt", "target/classes/largefile.txt"
+		checkFileSizeMatches "src/main/resources/regex.txt","target/classes/regex.txt"
+		
+		println "Checking contents"
+		new File("target/classes").eachFileMatch(~/.*\.txt/) { checkForTokensAndValues it.getPath() }
+		checkForTokensAndValues "target/classes/nesteddir/multiple-files1.txt"
+		checkForTokensAndValues "target/classes/nesteddir/multiple-files2.txt"
+		checkForTokensAndValues "target/classes/nesteddir-for-abs/multiple-files1.txt"
+		checkForTokensAndValues "target/classes/nesteddir-for-abs/multiple-files2.txt"
+		checkForTokensAndValues "target/simple-outputfile-remove.txt", "token", ""
+		checkForTokensAndValues "target/multiple-tokens-for-map-outputfile.txt", "token1", "value1"
+		checkForTokensAndValues "target/multiple-tokens-for-map-outputfile.txt", "token2", "value2"
+		checkForTokensAndValues "target/classes/newdir/simple-outputfile.txt"
+		checkForTokensAndValues "target/classes/special/multiple-tokens-to-replace.txt", "token1", "value1"
+		checkForTokensAndValues "target/classes/special/multiple-tokens-to-replace.txt", "token2", "value2"
+		checkForTokensAndValues "target/classes/include2"
+		checkForTokensAndValues "target/classes/file1"
+		checkForTokensAndValues "target/classes/file2"
+		checkForTokensAndValues "target/classes/outdir/simple.txt"
+		checkForTokensAndValues "target/classes/simple-absolute.txt"
+		checkForTokensAndValues "target/classes/linematch.txt", "replace=token", "replace=value"
+		checkForTokensAndValues "target/classes/special/variableTokenValueMap.txt", "token1", "value1"
+		checkForTokensAndValues "target/classes/special/variableTokenValueMap.txt", "token2", "value2"
+		checkForTokensAndValues "target/classes/target/outputDir/nesteddir/multiple-files2.txt", "token", "value"
+		checkForTokensAndValues "target/classes/timestamped.toReplace", "project.version", "0.0.1-SNAPSHOT"
+		checkForTokensAndValues "target/outputBasedir/outdir/simple.txt"
+		checkForTokensAndValues "target/xpath-out.xml"
+		checkForTokensAndValues "target/xpath-in-replacements-out.xml"
+		checkForTokensAndValues "target/classes/simple.txt.output"
+		checkForTokensAndValues "/tmp/simple.txt"
+	}
+}
